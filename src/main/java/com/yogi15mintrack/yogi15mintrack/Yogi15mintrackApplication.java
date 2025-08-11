7b054da99c2a82ea5e3a9a17054127fcafe213ee
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-08-11 18:32:25 +0200

    Initial commit

---
+++ src/main/java/com/yogi15mintrack/yogi15mintrack/Yogi15mintrackApplication.java
@@ -0,0 +1,13 @@
+package com.yogi15mintrack.yogi15mintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yogi15mintrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yogi15mintrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yogi15mintrack/yogi15mintrack/Yogi15mintrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogi15mintrack.yogi15mintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yogi15mintrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
