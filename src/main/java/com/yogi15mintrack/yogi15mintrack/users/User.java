bad7433826ca5296dd87f581a8e22d50a096da69
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-08-12 19:52:39 +0200

    feat: add User entity with fields and relations

---
+++ src/main/java/com/yogi15mintrack/yogi15mintrack/users/User.java
@@ -0,0 +1,41 @@
+package com.yogi15mintrack.yogi15mintrack.users;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+   // @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+   // private List<CompletedSession> completedSessions;
+
+    public boolean hasRole(Role role) {
+        return this.role == role;
+    }
+}
