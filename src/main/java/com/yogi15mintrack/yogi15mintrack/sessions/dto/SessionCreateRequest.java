7586aa1e69360f44f6554d8ad99524fe6ea90164
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-08-22 23:19:56 +0200

    fix(sessions): update service and add SessionUpdateRequest

---
+++ src/main/java/com/yogi15mintrack/yogi15mintrack/sessions/dto/SessionCreateRequest.java
@@ -0,0 +1,9 @@
+package com.yogi15mintrack.yogi15mintrack.sessions.dto;
+
+public record SessionCreateRequest(
+        String title,
+        String description,
+        String videoUrl,
+        int dayOfWeek
+) {
+}
