a82e46d9632dfd0be3b9d58940516de7f6391b2d
rozmarymethod@gmail.com <rozmarymethod@gmail.com>
2025-08-28 10:27:11 +0200

    fix(users): correct EntityNotFoundException import in UserService

--- src/main/java/com/yogi15mintrack/yogi15mintrack/config/SecurityConfig.java
+++ src/main/java/com/yogi15mintrack/yogi15mintrack/config/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
-import com.yogi15mintrack.yogi15mintrack.users.UserServiceImpl;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
